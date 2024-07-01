92709ea672686bbfa0c686b467b3756b45ab34d5
Suhan <Suhan@be-debugging.com>
2024-07-01 12:43:27 +0000

    consolidateVersion update

--- src/abrevioWebservice/ConsolidateRecordAsync.ts
+++ src/abrevioWebservice/ConsolidateRecordAsync.ts
@@ -1,33 +1,56 @@
-// const edge = require('edge-js');
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import edge from 'edge-js';
+import path from 'path';
+import logger from '../loggers/winstonLogger';
+import {
+    ConvertHL7V2TOV3Args,
+    ConvertHL7V2TOV3Result
+} from '../models/abrevioWebservice/convertHL7V2TOV3';
 
-// Load the .NET assembly function
-const ConsolidateRecordAsync = edge.func({
-    assemblyFile: 'DLLTEST.dll',
-    typeName: 'DLLTEST.RecordConsolidatorWrapper',
-    methodName: 'ConsolidateRecordAsync'
-});
+const consolidateVersion = async (
+) => {
+    // const { hl7v2 } = args;
+    logger.info('consolidateVersion');
 
-const consolidateVersion = async () => {
-    // Get the current time when starting
-    const startTime = new Date();
-    
+    const dllPath = path.join(
+        __dirname,
+        '..',
+        'abrevioWebservice',
+        'DLLs',
+        'DLLTEST.dll'
+    );
+    const ConsolidateRecordAsync = edge.func({
+        assemblyFile: 'DLLTEST.dll',
+        typeName: 'DLLTEST.RecordConsolidatorWrapper',
+        methodName: 'ConsolidateRecordAsync'
+    });
+
+    const v2CodeUpdateSetting = null;
+
     try {
-        console.log('Start Time:', startTime.toISOString());
-    
-        ConsolidateRecordAsync({}, (error: any, result: any) => {
-            if (error) {
-                console.error('Error:', error);
-            } else {
-                console.log('Record Consolidator Version:', result);
-            }
-            return {
-                consolidateVersionResult: result
-            };
+        const result = await new Promise<any>((resolve, reject) => {
+            ConsolidateRecordAsync({}, (error, result) => {
+                if (error) {
+                    console.error('Error:', error);
+                    reject(error);
+                } else {
+                    console.log('Record Consolidator Version:', result);
+                    resolve(result);
+                }
+            });
         });
-    } catch (error) {
-        console.log('Error in JS:', error);
+
+        logger.info(result);
+        return { consolidateVersionResult: result.Version };
+    } catch (error: any) {
+        logger.error(error);
+        const response: ConvertHL7V2TOV3Result = {
+            covertHL7V2TOV3Result: 'Failure',
+            version: '3.0',
+            error: error.message
+        };
+        return response;
     }
-}
+};
 
 export default consolidateVersion;
